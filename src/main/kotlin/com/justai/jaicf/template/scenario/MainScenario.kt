f8a80709fa5629ef062528ea007d111781c1fd8a
Denis Shurygin <sdi.linch@gmail.com>
2020-11-07 21:48:32 +0300

    Clear sessin on start

--- src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
+++ src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
@@ -20,6 +20,7 @@ object MainScenario : Scenario(
                 regex("/start")
             }
             action {
+                context.session.clear()
                 reactions.say("Привет, это Никита! Слушай, такое дело, у нас пожар в общаге! Срочно нужна твоя помощь! Что мне делать?")
                 reactions.go(StartRoomScenario.state)
             }
