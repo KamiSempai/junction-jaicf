bb75376c662b31b86be74b6161a4dd947f4ceead
Tinkturianec <zaynullin-bulat@yandex.ru>
2020-11-07 18:54:22 +0300

    add results

--- src/main/kotlin/com/justai/jaicf/template/scenario/EndGameScenario.kt
+++ src/main/kotlin/com/justai/jaicf/template/scenario/EndGameScenario.kt
@@ -1,6 +1,8 @@
 package com.justai.jaicf.template.scenario
 
 import com.justai.jaicf.model.scenario.Scenario
+import com.justai.jaicf.template.state.checkpoints
+import com.justai.jaicf.template.state.getReadableResults
 
 object EndGameScenario : Scenario() {
 
@@ -20,6 +22,24 @@ object EndGameScenario : Scenario() {
                 action {
                     reactions.say("Restarting the game")
                     reactions.go(MainScenario.state)
+                }
+            }
+
+            state("results") {
+                activators {
+                    regex("/results")
+                }
+
+                action {
+                    val (positive, negative) = context.checkpoints.getReadableResults()
+                    if (positive.isNotEmpty()) {
+                        reactions.say("В этих ситуациях вы поступили правильно:")
+                        positive.forEach { reactions.say(it) }
+                    }
+                    if (negative.isNotEmpty()) {
+                        reactions.say("В этих случаях вы ошиблись:")
+                        negative.forEach { reactions.say(it) }
+                    }
                 }
             }
         }
